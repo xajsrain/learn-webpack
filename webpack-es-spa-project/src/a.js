2a10b61b9ddc9c431140805ae19d5de5ee00d88c
rain-cool <1528067402@qq.com>
2021-11-30 00:43:28 +0800

    格式化

--- webpack-es-spa-project/src/a.js
+++ webpack-es-spa-project/src/a.js
@@ -1,10 +1,15 @@
 new Promise((resolve, reject) => {
-  resolve("333");
-});
+  resolve('333')
+})
 
 async function name() {
-  console.log("async 啥呀");
+  console.log('async 啥呀')
 }
 
-name();
-console.log("123");
+name()
+console.log('123')
+console.log([
+  'hellhellhellhellhellhell',
+  'sheneshenesheneshenesheneshene',
+  123132,
+])
